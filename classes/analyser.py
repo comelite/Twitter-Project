5179bf5849b863d8240d786e9c1cd0200d33d9cd
XL85 <86225232+XL85@users.noreply.github.com>
2022-12-23 17:51:51 +0100

    Ajout_racisme
    Ajout d'une classe sur le racisme

--- classes/analyser.py
+++ classes/analyser.py
@@ -5,15 +5,20 @@ from nltk.stem import WordNetLemmatizer
 from nltk.corpus import stopwords
 from PIL import Image
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import re
 import string
 from wordcloud import WordCloud
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import f1_score
+
 download('stopwords')
 download('wordnet')
 download('omw-1.4')
-
 
 class Cloud():
 
@@ -88,3 +93,51 @@ class Sentiment():
         afinn = Afinn()
         score = afinn.score(tweet)
         return score >= 0
+    
+class Racist():
+    # Sentiment analysis class
+    def __init__(self):
+        pass
+            
+    def init_racism(self):    
+        data_init = pd.read_csv("data_init.csv")
+        data_init.fillna("", inplace=True)
+        stoplist = set(stopwords.words('french'))
+        vectorizer = TfidfVectorizer(stop_words = list(stoplist), ngram_range=(1, 3), min_df=10)
+        features = vectorizer.fit_transform(data_init.tweet)
+        X_train, X_test, y_train, y_test = train_test_split(features, data_init.label)
+        params = {'penalty': ['l1', 'l2'], 'C': [3, 10, 30, 100, 300]}
+        lrmodel = GridSearchCV(LogisticRegression(solver='liblinear', max_iter=250), param_grid=params, scoring='f1', cv=5, n_jobs=-1)
+        lrmodel.fit(X_train, y_train)
+        best_model = LogisticRegression(solver = 'liblinear', max_iter=250, C = lrmodel.best_params_['C'], penalty = lrmodel.best_params_['penalty'])
+        best_model.fit(X_train, y_train)
+
+        probas = best_model.predict_proba(X_test)
+        thresholds = np.arange(0.1, 0.9, 0.01)
+        scores = [f1_score(y_test, (probas[:, 1] >= x).astype(int)) for x in thresholds]
+        best_threshold = thresholds[np.argmax(scores)]
+        return best_model, vectorizer, best_threshold
+    
+    def clean_tweet(self, tweet):
+        temp = tweet.lower()
+        temp = re.sub("@[A-Za-z0-9_]+","", temp)
+        temp = re.sub("#[A-Za-z0-9_]+","", temp)
+        temp = re.sub(r'http\S+', '', temp)
+        temp = re.sub('[()!?]', ' ', temp)
+        temp = re.sub('\[.*?\]',' ', temp)
+        temp = re.sub("[^a-zàâçéèêëîïôûùæœ0-9]"," ", temp)
+        temp = temp.split()
+        stoplist = list(set(stopwords.words('french')))
+        temp = [w for w in temp if not w in stoplist]
+        temp = " ".join(word for word in temp)
+        return temp
+    
+    def tweet_to_racism(self, tweet, best_model, vectorizer, best_threshold):
+        # Racist tone analysis of a tweet
+        # Returns True if the tweet has a racist tone, and the probability
+        new_features = vectorizer.transform([tweet])
+        proba = best_model.predict_proba(new_features)[0][1]
+        racist = False
+        if proba > best_threshold + 0.3 :
+            racist = True
+        return racist, proba #print (f'Racist tone with a {np.round(proba*100,1)}% probability')
--- main.py
+++ main.py
@@ -1,31 +1,39 @@
 # import classes from the class folder
-from classes import secrets, ingestor, retriever, analyser
+from classes import secret, ingestor, retriever, analyser
 
 if __name__ == "__main__":
 
     # Create a class to decode the secrets
-    keys = secrets.Secrets()
+    keys = secret.Secret()
 
     # Create a class to analyse the tweets
     feed = ingestor.Ingestor(keys.bearer_token)
 
     # Get 10 recent tweets from the topic "france"
-    tweets = feed.get_recent_tweets("france", 10)
+    tweets = feed.get_recent_tweets("football", 10)
 
     # Send them to the kafka topic "test_tweets"
-    feed.send_to_kafka(tweets, "test_tweets", "en", False)
+    feed.send_to_kafka(tweets, "test_football", "fr", False)
 
     # Retrieve the tweets from the kafka topic "tweets" as a generator
-    test_tweets = retriever.Retriever("test_tweets")
+    test_tweets = retriever.Retriever("test_football")
     test_tweets_generator = test_tweets.retrieve_tweets(10)
 
     # Analyse the tweets
     at = analyser.Cloud()
     sentiment = analyser.Sentiment()
+    racism = analyser.Racist()
 
+    best_model, vectorizer, best_threshold = racism.init_racism()
+    
     for tweet in test_tweets_generator:
-        at.tweet_to_tokens(tweet, "english")
+        at.tweet_to_tokens(tweet, "french")
         print(tweet)
+        cleaned_tweet = racism.clean_tweet(tweet)
+        print(cleaned_tweet)
         print("Sentiment: ", "positive" if sentiment.tweet_to_sentiment(
-            tweet) else "negative")
+            cleaned_tweet) else "negative")
+        racist, proba = racism.tweet_to_racism(cleaned_tweet, best_model, vectorizer, best_threshold)
+        if racist:
+            print (f'Racist tone with a {int(proba*100)}% probability')
     at.most_common_token_to_img()
