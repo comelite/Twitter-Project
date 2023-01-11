11cd4c0409af6de34a0129af2f2513781017f803
XL85 <86225232+XL85@users.noreply.github.com>
2023-01-11 08:05:11 +0100

    Modifs to abide by Flake8

--- classes/analyser.py
+++ classes/analyser.py
@@ -1,7 +1,8 @@
-from PIL import Image
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+from PIL import Image
 from wordcloud import WordCloud
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -12,16 +13,20 @@ from sklearn.metrics import f1_score
 from nltk.corpus import stopwords
 
 from classes.cleaner import Cleaner
-        
+
+
 class Cloud():
 
-    def __init__(self, word_window = 200):
+    def __init__(self, word_window=200):
         self.tokens = []
         self.word_window = word_window
-        
+
     def word_windowing(self):
-        # Word windowing, if the list of words is longer than the word window, then we cut it
-        self.tokens = self.tokens if len(self.tokens) <= self.word_window else self.tokens[-self.word_window:]
+        # Word windowing
+        # if list of words longer than word window, we cut it
+        self.tokens = (self.tokens
+                       if len(self.tokens) <= self.word_window
+                       else self.tokens[-self.word_window:])
 
     def tweet_to_tokens(self, tweet, lang):
         # Convert the tweet to tokens
@@ -35,64 +40,91 @@ class Cloud():
         # Generate a wordcloud image from the most common tokens
         total_sentences = " ".join(self.tokens)
         twitter_mask = np.array(Image.open("img/twitter.jpg"))
-        wordcloud = WordCloud(width=800, height=500, random_state=42, max_font_size=100, mask=twitter_mask,
-                              contour_color="steelblue", contour_width=0, background_color="white").generate(total_sentences)
+        wordcloud = WordCloud(width=800,
+                              height=500,
+                              random_state=42,
+                              max_font_size=100,
+                              mask=twitter_mask,
+                              contour_color="steelblue",
+                              contour_width=0,
+                              background_color="white").generate(
+                                  total_sentences)
         plt.imshow(wordcloud, interpolation='bilinear')
         plt.axis('off')
         plt.show()
-    
+
+
 class Racist():
     # Sentiment analysis class
-    def __init__(self, path, params = {'penalty': ['l1', 'l2'], 'C': [3, 10, 30, 100, 300]}):
+    def __init__(self, path, params={'penalty': ['l1', 'l2'],
+                                     'C': [3, 10, 30, 100, 300]}):
         # Init the racist classifier
         # @param path : Full path to the dataset
         # @param params : Parameters to use for the GridSearch
-        
+
         # Load the dataset & fill the empty values
         self.dataset = pd.read_csv(path).fillna('')
-                
+
         # if path does not contain fr then it is english
         self.lang = 'french' if 'fr' in path else 'english'
-        
+
         # Set the stopwords to the language of the dataset
         self.stoplist = stopwords.words(self.lang)
-        
+
         # Set the vectorizer
-        self.vectorizer = TfidfVectorizer(stop_words = self.stoplist, ngram_range=(1, 3), min_df=10)       
-        
+        self.vectorizer = TfidfVectorizer(
+            stop_words=self.stoplist,
+            ngram_range=(1, 3), min_df=10)
+
         features = self.vectorizer.fit_transform(self.dataset.tweet)
-        X_train, X_test, y_train, y_test = train_test_split(features, self.dataset.label)
-        
+        X_train, X_test, y_train, y_test = train_test_split(
+            features,
+            self.dataset.label)
+
         # GridSearch to find the best parameters for the model
-        optimal_params = GridSearchCV(LogisticRegression(solver='liblinear', max_iter=250), param_grid=params, scoring='f1', cv=5, n_jobs=-1)
+        optimal_params = GridSearchCV(
+            LogisticRegression(
+                solver='liblinear',
+                max_iter=250),
+            param_grid=params,
+            scoring='f1',
+            cv=5,
+            n_jobs=-1)
         optimal_params.fit(X_train, y_train)
-        
+
         # Train the model with the best parameters
-        best_model = LogisticRegression(solver = 'liblinear', max_iter=250, C = optimal_params.best_params_['C'], penalty = optimal_params.best_params_['penalty'])
+        best_model = (
+            LogisticRegression(
+                solver='liblinear',
+                max_iter=250,
+                C=optimal_params.best_params_['C'],
+                penalty=optimal_params.best_params_['penalty']))
         best_model.fit(X_train, y_train)
-        
+
         # Find the best threshold
         probas = best_model.predict_proba(X_test)
         thresholds = np.arange(0.1, 0.9, 0.01)
-        
+
         # Calculate f1 score for each threshold
-        scores = [f1_score(y_test, (probas[:, 1] >= x).astype(int)) for x in thresholds]
+        scores = ([f1_score(y_test,
+                            (probas[:, 1] >= x).astype(int))
+                   for x in thresholds])
         best_threshold = thresholds[np.argmax(scores)]
-        
+
         # Set the model and the threshold
         self.model = best_model
         self.threshold = best_threshold
-    
+
     def tweet_to_racism(self, tweet):
         # Racist tone analysis of a tweet
         # Returns True if the tweet has a racist tone, and the probability
-        
+
         cleaner = Cleaner(tweet, self.lang, self.stoplist)
         tweet = cleaner.to_tokens()
-        
+
         new_features = self.vectorizer.transform([tweet])
         proba = self.model.predict_proba(new_features)[0][1]
         racist = False
-        if proba > self.threshold + 0.2 :
+        if proba > self.threshold + 0.2:
             racist = True
         return racist, proba
--- classes/user_information.py
+++ classes/user_information.py
@@ -1,7 +1,7 @@
 import tweepy
-import datetime
-import time
+
 from classes import ingestor
+
 
 class User_Information():
     def __init__(self, bearer_token):
@@ -9,18 +9,22 @@ class User_Information():
         # @param bearer_token : the bearer token to access the twitter API
         self.bearer_token = bearer_token
         self.client = tweepy.Client(bearer_token=bearer_token)
-        
+
     def get_user_tweets(self, user_id, limit):
         # Get the recent tweets from the twitter API
         # @param user_id : the id of the user to get the tweets from
         # @param limit : the number of tweets to get
         feed = ingestor.Ingestor(self.bearer_token)
-        tweets = self.client.get_users_tweets(user_id, tweet_fields=['context_annotations', 'created_at', 'lang'], max_results=limit)
+        tweets = self.client.get_users_tweets(
+            user_id,
+            tweet_fields=['context_annotations',
+                          'created_at', 'lang'],
+            max_results=limit)
         # Send the tweets to the kafka topic user_id_tweets
         topic = f"{user_id}_tweets"
         feed.send_to_kafka_from_dict(tweets.data, topic)
         return topic
-    
+
     def get_user_relations(self, user_id, limit):
         # Get the recent tweets from the twitter API
         # @param user_id : the id of the user to get the tweets from
@@ -32,12 +36,12 @@ class User_Information():
         # Get the recent tweets from the twitter API
         # @param username : the username of the user to get the tweets from
         # @param limit : the number of tweets to get
-        user = self.client.get_user(username = username)
+        user = self.client.get_user(username=username)
         return user
 
     def get_user_information_from_id(self, id):
         # Get the recent tweets from the twitter API
         # @param id : the id of the user to get the tweets from
         # @param limit : the number of tweets to get
-        user = self.client.get_user(id = id)
+        user = self.client.get_user(id=id)
         return user.data
